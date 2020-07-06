736e50174de4672bbaa907a16d5cfc21a7b6e416
dvfleet413 <dvfleet413@gmail.com>
2020-07-05 23:10:09 -0400

    fixed bug causing destruction of form entries

--- static/entree.js
+++ static/entree.js
@@ -1,18 +1,19 @@
 const addWine = () => {
-    console.log(event)
-      event.preventDefault()
-      winesContainer = document.querySelector('#wines-container')
-      winesContainer.innerHTML += `
-          <label for="wines">Wine Pairing</label>
-          <input name="wines" id="wines" />
-          <button onclick="addWine()">Add Wine</button><br />`
+    event.preventDefault()
+    const newField = document.createElement('div')
+    newField.innerHTML = `
+        <label for="wines">Wine Pairing</label>
+        <input name="wines" id="wines" />
+        <button onclick="addWine()">Add Wine</button><br />`
+    document.querySelector('#wines-container').appendChild(newField)
 }
 
 const addAllergen = () => {
-      event.preventDefault()
-      allergensContainer = document.querySelector('#allergens-container')
-      allergensContainer.innerHTML += `
-          <label for="allergens">Allergen</label>
-          <input name="allergens" id="allergens" />
-          <button onclick="addAllergen()">Add Allergen</button><br />`
+    event.preventDefault()
+    const newField = document.createElement('div')
+    newField.innerHTML = `
+        <label for="allergens">Allergen</label>
+        <input name="Allergens" id="allergens" />
+        <button onclick="addAllergen()">Add Allergen</button><br />`
+    document.querySelector('#allergens-container').appendChild(newField)
 }
