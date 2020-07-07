19bab161c856dfab561a85b08025432566f3e847
dvfleet413 <dvfleet413@gmail.com>
2020-07-07 10:49:56 -0400

    working on admin panel

--- static/entree.js
+++ static/entree.js
@@ -2,9 +2,12 @@ const addWine = () => {
     event.preventDefault()
     const newField = document.createElement('div')
     newField.innerHTML = `
-        <label for="wines">Wine Pairing</label>
-        <input name="wines" id="wines" />
-        <button onclick="addWine()">Add Wine</button><br />`
+        <div>
+            <label for="wines">Wine Pairing</label>
+            <input name="wines" id="wines" />
+            <button onclick="addWine()">+</button>
+            <button onclick="removeItem()">-</button><br />
+        </div>`
     document.querySelector('#wines-container').appendChild(newField)
 }
 
@@ -12,8 +15,16 @@ const addAllergen = () => {
     event.preventDefault()
     const newField = document.createElement('div')
     newField.innerHTML = `
-        <label for="allergens">Allergen</label>
-        <input name="Allergens" id="allergens" />
-        <button onclick="addAllergen()">Add Allergen</button><br />`
+        <div>
+            <label for="allergens">Allergen</label>
+            <input name="Allergens" id="allergens" />
+            <button onclick="addAllergen()">+</button>
+            <button onclick="removeItem()">-</button><br />
+        </div>`
     document.querySelector('#allergens-container').appendChild(newField)
+}
+
+const removeItem = () => {
+    event.preventDefault()
+    event.target.parentElement.remove()
 }
