04191c8767fbf93e7282329073a4c1cb6f30667f
dvfleet413 <dvfleet413@gmail.com>
2020-07-07 11:14:57 -0400

    refactoring for heroku deployment

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask_restful import Api
 from resources.routes import initialize_routes
 
 app = Flask(__name__, instance_relative_config=True)
-app.config.from_pyfile('config.py')
 api = Api(app)
 
 app.config['MONGODB_SETTINGS'] = {
