26e3254c2cb4a3e2152b6f24b3909b136fcdcdf7
dvfleet413 <dvfleet413@gmail.com>
2020-07-07 11:31:08 -0400

    configuration for deployment

--- database/db.py
+++ database/db.py
@@ -1,7 +1,17 @@
 from flask_mongoengine import MongoEngine
+import click
+from flask.cli import with_appcontext
 
 db = MongoEngine()
 
 
 def initialize_db(app):
     db.init_app(app)
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    initialize_db()
+    click.echo('Initialized the database.')
